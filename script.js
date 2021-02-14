9fd3736fb804e5e56c6b2aa3173c8fac145b9de8
dino <woo.dong.7.wd@gmail.com>
2021-02-14 15:15:33 +0100

    complit

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ let data = {    firstNum: "",
                 firstSambol: "",
                 secondNum: "",
                 result: false}
+                
+function write(str) {
+    document.querySelector("#imput").value = str; 
+}
+
+function clear(data)
+{
+    data.firstNum = "";
+    data.secondNum = "";
+    data.firstSambol = "";
+    data.secondNum = "";
+    data.result= false;
+    
+}
+
+function del()
+{
+    clear(data);
+    write("");
+}
 
 function getSambol(sambol) {
     if (!data.firstSambol)
@@ -16,63 +36,53 @@ function getSambol(sambol) {
 
 function getNum(nb) {
     if (!data.firstSambol && data.result == false)
-        data.firstNum = data.firstNum + nb;
+    {   data.firstNum = data.firstNum + nb;
+        write(data.firstNum);
+    }
     else
-        data.secondNum = data.secondNum + nb;
-}
-
-function clear(data)
+    {   data.secondNum = data.secondNum + nb;
+        write(data.secondNum);
+    }
+}   
+   
+function init(data)
 {
-    data.firstNum = "";
-    data.secondNum = "";
-    data.firstSambol = "";
-    data.secondNum = "";
-    data.result= false;
-}
-
-function del()
-{
-    clear(data);
-}
-
- function init(data)
- {
     data.secondNum ="";
     data.firstSambol = (data.secondSambol != '=') ? (data.secondSambol) : ("");
     data.secondSambol ="";
- }
-
+}
+    
 function calculator(data) {
     
     if (data.secondSambol && data.firstSambol != "=" && data.secondNum)
     {
         let str = data.firstNum + data.firstSambol + data.secondNum;
         data.firstNum = eval(str);
-        console.log(str + " = " + data.firstNum);
+        write(data.firstNum);
         init(data);
         data.result = true;
     }
     else{
         clear(data);
-        console.log("Error");
+        write("Error");
     }
 }
 
 function fromKey()
 { 
-    document.addEventListener('keypress', (event) => {
+    document.addEventListener('keyup', (event) => {
         const keyName = event.key;
-        if (keyName >= 0 && keyName <= 9)
+        if ((keyName >= 0 && keyName <= 9) || keyName === '.')
             getNum(keyName);
         else if (keyName === "/" || keyName === "*" || keyName === "-" || keyName === "+")
             getSambol(keyName);
         else if (keyName === "=" || keyName === "Enter")
             getSambol("=");
-        else if (keyName === '.')
-        {
-            // console.log(".");
+        else if (keyName === "Backspace") {
+            write("");
+            del();
         }
-      }, false);
+    }, false);
 }
 
 function main()
